30c04c0c6b3d3b08258067330efb3f85ffd36c3a
mohanex <mohamedpack@gmail.com>
2023-05-22 11:19:41 +0200

    On branch main

---
+++ LVDS_UART_PLATFORM/platform.tcl
@@ -0,0 +1,19 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\mafassi\Desktop\UART_LVDS_Vitis\LVDS_UART_PLATFORM\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\mafassi\Desktop\UART_LVDS_Vitis\LVDS_UART_PLATFORM\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {LVDS_UART_PLATFORM}\
+-hw {C:\Users\mafassi\Downloads\carte_blowbox\blowbox\Microblaze5\Microblaze_LVDS.xsa}\
+-proc {microblaze_0} -os {standalone} -fsbl-target {psu_cortexa53_0} -out {C:/Users/mafassi/Desktop/UART_LVDS_Vitis}
+
+platform write
+platform generate -domains 
+platform active {LVDS_UART_PLATFORM}
+platform generate
+platform write
---
+++ Microblaze_LVDS/platform.tcl
@@ -0,0 +1,19 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\mafassi\Desktop\UART_LVDS_Vitis\Microblaze_LVDS\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\mafassi\Desktop\UART_LVDS_Vitis\Microblaze_LVDS\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {Microblaze_LVDS}\
+-hw {C:\Users\mafassi\Downloads\carte_blowbox\blowbox\Microblaze5\Microblaze_LVDS.xsa}\
+-fsbl-target {psu_cortexa53_0} -out {C:/Users/mafassi/Desktop/UART_LVDS_Vitis}
+
+platform write
+domain create -name {standalone_microblaze_0} -display-name {standalone_microblaze_0} -os {standalone} -proc {microblaze_0} -runtime {cpp} -arch {32-bit} -support-app {hello_world}
+platform generate -domains 
+platform active {Microblaze_LVDS}
+platform generate -quick
