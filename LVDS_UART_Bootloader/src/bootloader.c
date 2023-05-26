ccdf59983c94fa7272b54bffd6ded6945e41c9e4
mohanex <mohamedpack@gmail.com>
2023-05-26 11:07:05 +0200

    v1.34-debugging1.3-bootloader_change

--- LVDS_UART_Bootloader/src/bootloader.c
+++ LVDS_UART_Bootloader/src/bootloader.c
@@ -41,7 +41,7 @@
 #define RECORD_TERMINATOR	2
 
 /* Comment the following line, if you want a smaller and faster bootloader which will be silent */
-#define VERBOSE
+//#define VERBOSE
 
 /* Declarations */
 static void display_progress (uint32_t lines);
@@ -141,7 +141,7 @@ static uint8_t sr_data_buf[SREC_DATA_MAX_BYTES];
 u32 flbuf;
 
 #ifdef VERBOSE
-static int8_t errors[] = {
+static int8_t *errors[] = {
 	"",
 	"Error while copying executable image into RAM",
 	"Error while reading an SREC line from flash",
